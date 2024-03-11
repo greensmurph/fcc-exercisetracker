7c6ab01fe64b2a055929b40d5bbb986c3b255324
Jonathan Self <jon.self@live.com>
2024-03-10 23:59:25 -0500

    Add new user post call

--- index.js
+++ index.js
@@ -3,8 +3,19 @@ const app = express()
 const cors = require('cors')
 require('dotenv').config()
 const bodyParser = require('body-parser');
+let mongoose = require('mongoose');
 
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false
+});
 
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true }
+});
+
+let User = mongoose.model('User', userSchema);
 
 app.use(cors())
 app.use(bodyParser.urlencoded({extended: false}));
@@ -13,12 +24,14 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
-// app.route("/api/users")
-//   //.get()
-//   .post((res, req) => {
-//     const user = req.body.username;
-    
-//   })
+app.route('/api/users')
+  .post((req, res) => {
+    const username = req.body.username;
+    let newUser = new User({username: username});
+    newUser.save((err, data) => {
+      if (!err) res.send({username: data.username, _id: data._id});
+    });
+  });
 
 
 
